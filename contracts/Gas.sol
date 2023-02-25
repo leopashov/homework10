72caa146c2a065cad67c5e201bad0d2615d15525
Michael Reynolds <103044686+mireynolds@users.noreply.github.com>
2023-02-25 17:46:24 +0000

    Assembly for whitelist / administrator functions and constructor

--- contracts/Gas.sol
+++ contracts/Gas.sol
@@ -3,15 +3,7 @@ pragma solidity 0.8.19;
 
 contract GasContract {
     mapping(address => uint256) private balances; // 0x0
-    uint256 public constant totalSupply = 10000;
     address private immutable owner;
-    address private constant wAdd1 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
-    address private constant wAdd2 = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
-    address private constant wAdd3 = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
-    address private constant admin1 = 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2;
-    address private constant admin2 = 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46;
-    address private constant admin3 = 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf;
-    address private constant admin4 = 0xeadb3d065f8d15cc05e92594523516aD36d1c834;
 
     event Transfer(address recipient, uint256 amount);
 
@@ -27,29 +19,40 @@ contract GasContract {
         uint256 amount;
     }
 
-    constructor(address[5] memory, uint16) payable {
-        balances[msg.sender] = totalSupply;
+    constructor(address[5] memory, uint16) {
+        assembly {
+            // Calculate slot
+            mstore(0, caller())
+            let senderSlot := keccak256(0, 0x40)
+            // Update balance
+            sstore(senderSlot, sub(sload(senderSlot), 10000))
+        }
         owner = msg.sender;
     }
 
-    function administrators (uint256 i) public view returns (address) {
-        if (i == 0) {return admin1;}
-        if (i == 1) {return admin2;}
-        if (i == 2) {return admin3;}
-        if (i == 3) {return admin4;}
-        if (i == 4) {return owner;}
-        else {
-            revert();
+    function administrators (uint256 i) public view returns (address addr) {
+        address _owner = owner;
+        assembly {
+            if eq(i,0) {addr := 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2}
+            if eq(i,1) {addr := 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46}
+            if eq(i,2) {addr := 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf}
+            if eq(i,3) {addr := 0xeadb3d065f8d15cc05e92594523516aD36d1c834}
+            if eq(i,4) {addr := _owner}
         }
+        return addr;
     }
 
-    function whitelist (address addr) public pure returns (uint256) {
-        if (addr == wAdd1) { return 1; }
-        if (addr == wAdd2) { return 2; }
-        if (addr == wAdd3) { return 3; }
-        else {
-            revert();
+    function whitelist (address addr) public view returns (uint256 num) {
+        uint8 firstLetter = uint8(bytes1(bytes20(addr)));
+        assembly {
+            //calldatacopy(31, 16, 1)
+            //let firstLetter := mload(0)
+            num := 0
+            if eq(firstLetter, 0x70) { num := 1 }
+            if eq(firstLetter, 0x3C) { num := 2 }
+            if eq(firstLetter, 0x90) { num := 3 }
         }
+        return num;
     }
 
     function transfer(
@@ -98,6 +101,10 @@ contract GasContract {
 
     function getTradingMode() external pure returns (bool) {
         return true;
+    }
+
+    function totalSupply() external pure returns (uint256) {
+        return 10000;
     }
 
     function whiteTransfer(
